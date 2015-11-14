43cb0436e47733e3485832024682abc0f17dc0d1
Ethan Sargent <ethan.sargent.1250@gmail.com>
2015-11-13 21:21:49 -0800

    added train method

---
+++ logger/logger.py
@@ -0,0 +1,13 @@
+
+    def train(self):
+        """ trains the weight vector on the training data
+        """
+        alpha = self._alpha
+        
+        # going through the training data _maxIter times
+        for i in range(_maxIter):
+        
+            # moving through the training data
+            for k in range(self._trainingData):
+                self._weight
+        
--- logisticRegression.py
+++ logisticRegression.py
@@ -29,6 +29,26 @@ class LogReg(self):
         Takes the dot product of one feature and the weight vector
         and returns the result of them entered into the sigmoid function
         """
-
-
+        
+    def train(self):
+        """ trains the weight vector on the training data
+        """
+        alpha = self._alpha
+        
+        # going through the training data _maxIter times
+        for iter in range(_maxIter):
+        
+            # moving through the training data
+            for i in range(self._data.size):
+                xi = self._data[i]
+                # the predicted value
+                hxi = self.predict(xi)
+                # the target value
+                yi = self._tags[i]
+                
+                # calculating the delta by which to adjust this particular weight
+                delta = alpha * (hxi - yi) * xi
+                
+                self._weight[k] -= delta 
+        
 
